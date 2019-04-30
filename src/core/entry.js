4ad5c9888cb11c42959a86df9f68f2808dc88bb1
OwlAford <244734834@qq.com>
2019-04-30 17:33:44 +0800

    fix some problems about service-worker

--- src/core/entry.js
+++ src/core/entry.js
@@ -1,3 +1,4 @@
+import registerServiceWorker from "./registerServiceWorker";
 import "~/utils/raf";
 import React from "react";
 import { render } from "react-dom";
@@ -18,3 +19,5 @@ render(
   </Provider>,
   document.getElementById("MOUNT_NODE")
 );
+
+registerServiceWorker();
